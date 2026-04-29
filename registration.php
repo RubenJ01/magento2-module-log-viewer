665dc89ddb89bc01dbe277e6f455ea50d1a9398d
Ruben <45236226+RubenJ01@users.noreply.github.com>
2026-04-29 10:45:20 +0200

    fix(registration): include php-humanize in Magento DI compilation (#18)

--- registration.php
+++ registration.php
@@ -2,6 +2,21 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
+/*
+ * Register the rjds/php-humanize library as a Magento "library" component so
+ * that its classes are picked up by `bin/magento setup:di:compile`. Without
+ * this, the <preference> in etc/di.xml for Rjds\PhpHumanize\HumanizerInterface
+ * fails compilation with "the latter has not been included in dependency
+ * injection compilation."
+ */
+$humanizerPath = \dirname(__DIR__) . '/php-humanize';
+$registrar = new ComponentRegistrar();
+if (\is_dir($humanizerPath)
+    && $registrar->getPath(ComponentRegistrar::LIBRARY, 'rjds/php-humanize') === null
+) {
+    ComponentRegistrar::register(ComponentRegistrar::LIBRARY, 'rjds/php-humanize', $humanizerPath);
+}
+
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
     'RJDS_LogViewer',
